cfca18304007e4b3b0fa508356a923b16aecc7e0
jfmsantos <fabricioj053@gmail.com>
2022-12-30 02:15:43 -0300

    Aterar regra tranfereValor

--- src/br/com/negocio/conta/GerenciadoraContas.java
+++ src/br/com/negocio/conta/GerenciadoraContas.java
@@ -14,43 +14,43 @@ public class GerenciadoraContas {
 		return contasDoBanco;
 	}
 
-	public ContaCorrente pesquisaConta (int idConta) {
+	public ContaCorrente pesquisaConta(int idConta) {
 
 		return contasDoBanco.stream()
-				.filter(conta-> conta.getId() == idConta)
+				.filter(conta -> conta.getId() == idConta)
 				.findFirst()
 				.orElse(null);
 	}
-	
-	public void adicionaConta (ContaCorrente novaConta) {
+
+	public void adicionaConta(ContaCorrente novaConta) {
 		this.contasDoBanco.add(novaConta);
 	}
 
-	public boolean removeConta (int idConta) {
-		
+	public boolean removeConta(int idConta) {
+
 		return contasDoBanco.removeIf(conta -> conta.getId() == idConta);
 	}
 
-	public boolean contaAtiva (int idConta) {
-		
+	public boolean contaAtiva(int idConta) {
+
 		return contasDoBanco.stream()
-				.filter(conta -> conta.getId()==idConta)
+				.filter(conta -> conta.getId() == idConta)
 				.anyMatch(ContaCorrente::isAtiva);
 	}
-	
-	public boolean transfereValor (int idContaOrigem, double valor, int idContaDestino) {
-		
+
+	public boolean transfereValor(int idContaOrigem, double valor, int idContaDestino) {
+
 		boolean sucesso = false;
-		
+
 		ContaCorrente contaOrigem = pesquisaConta(idContaOrigem);
 		ContaCorrente contaDestino = pesquisaConta(idContaDestino);
-		
-		if(contaOrigem.getSaldo() >= valor){
-			contaDestino.setSaldo(contaDestino.getSaldo() + valor);
-			contaOrigem.setSaldo(contaOrigem.getSaldo() - valor);
-			sucesso = true;
-		}
-	
+
+		// if(contaOrigem.getSaldo() >= valor){
+		contaDestino.setSaldo(contaDestino.getSaldo() + valor);
+		contaOrigem.setSaldo(contaOrigem.getSaldo() - valor);
+		sucesso = true;
+		// }
+
 		return sucesso;
 	}
 }
